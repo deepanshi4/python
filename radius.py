0e25b348007bdb98e24518dc64f7785704a89c44
deepanshi4 <79511955+deepanshi4@users.noreply.github.com>
2021-08-17 16:05:17 +0530

    radius.py
    a Python program which accepts the radius of a circle from the user and computes area.

---
+++ radius.py
@@ -0,0 +1,4 @@
+# python
+write a Python program which acfrom math import pi
+r = float(input("Enter the radius of circle:"))
+print("The Area of the circle with radius is :" +str(pi*r*r))cepts the radius of a circle from the user and computes area.
